da7a1024bc0e514022416402f92c008cf3d45c0e
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-09 12:29:14 +0000

    made fullscreen.js work with Safari

--- src/Components/Menu/Fullscreen.js
+++ src/Components/Menu/Fullscreen.js
@@ -2,26 +2,25 @@ import React from 'react'
 import fullscreenClose from '../../Images/fullscreenClose.png'
 import fullscreenOpen from '../../Images/fullscreenOpen.png'
 
+const root = document.getElementById('root')
+
 const toggleFullscreen = () => {
-    if (document.fullscreenEnabled) {
-        document.exitFullscreen()
-            .then(()=>{
-                document.getElementById('fullscreenButton').src=fullscreenOpen
-            })
+    if (document.webkitIsFullScreen) {
+        console.log('hello')
+        document.webkitExitFullscreen()
+        document.getElementById('fullscreenButton').src=fullscreenOpen
     } else {
-        document.getElementById('root').requestFullscreen()
-            .then(()=>{
-            document.getElementById('fullscreenButton').src=fullscreenClose
-        })
-    }
-};
+        console.log('goodbye')
+        root.webkitRequestFullscreen()
+        document.getElementById('fullscreenButton').src=fullscreenClose
+        }
+}
 
 
 const Fullscreen = () => {
 
-
     return (
-        <img id="fullscreenButton" src={fullscreenOpen} onClick={toggleFullscreen} />
+        <img id="fullscreenButton" height="100px" src={fullscreenOpen} onClick={toggleFullscreen} />
 
     )
 }
