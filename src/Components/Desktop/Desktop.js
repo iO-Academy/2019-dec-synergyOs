319f3674473f620c6b6f5dab5b733e5f1755c658
jdselby24 <jdselby24@gmail.com>
2019-12-09 12:46:57 +0000

    added logo to desktop

--- src/Components/Desktop/Desktop.js
+++ src/Components/Desktop/Desktop.js
@@ -8,7 +8,7 @@ class Desktop extends Component {
             width: '100%',
             height: '100vh',
             backgroundColor: this.props.background,
-            backgroundImage: ` url${this.props.backgroundImg})`,
+            backgroundImage: ` url(${this.props.backgroundImg})`,
             backgroundRepeat: `no-repeat`,
             backgroundAttachment: `fixed`,
             backgroundPosition: `center`
