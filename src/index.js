642bc164f6ea18cfccb6cd3c7b1e8450406c2ee4
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-09 11:13:04 +0000

    changed index.js to render Clock.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clock from "./Components/Menu/Clock";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
