158ac1dbe02627d5574f3fa66d607f427b615a58
Anton Webb <anton25360@gmail.com>
2019-12-09 11:17:30 +0000

    Implemented Menu bar

---
+++ src/Components/MenuBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MenuBar.css';
+
+function MenuBar() {
+  return (
+    <div className="MenuBar">
+      <h1>add menu components here</h1>
+    </div>
+  )
+}
+
+export default MenuBar;
