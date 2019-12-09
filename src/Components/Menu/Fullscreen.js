c8964d3fb0163be6a26dbd5e7c9eb9ed776f8d79
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-09 11:35:43 +0000

    added placeholder code for fullscreen component

--- src/Components/Menu/Fullscreen.js
+++ src/Components/Menu/Fullscreen.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import logo from "../../../../Ex1createReactApp/testapp/src/logo.svg";
 
-
+const Fullscreen = () => {
+    return (
+        <h1 align="center" onClick={this.handleClick}>Click me!</h1>
+    )
+}
 
 export default Fullscreen
