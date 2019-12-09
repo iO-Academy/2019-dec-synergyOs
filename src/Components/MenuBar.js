f81711d467ffcf94b1bc05e00229f2be18f6aadd
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-09 12:07:39 +0000

    added fullscreen button to menu bar

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './MenuBar.css';
+import Fullscreen from "./Menu/Fullscreen";
 
 function MenuBar() {
   return (
     <div className="MenuBar">
+        <Fullscreen/>
       <h1>add menu components here</h1>
     </div>
   )
