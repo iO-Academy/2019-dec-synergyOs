76e118ee877d0698d6f58167317de79cc97a88ab
jdselby24 <jdselby24@gmail.com>
2019-12-09 14:28:54 +0000

    Added state value for storing open app names

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import MenuBar from './Components/MenuBar'
 import Desktop from './Components/Desktop/Desktop';
 import logo from './Images/synergyoslogo.png';
+import { AppContext } from './AppStateContext';
 
-function App() {
-  return (
-    <div>
-      <Desktop background='#D8AEB6' backgroundImg={logo}/>
-      <MenuBar />
-    </div>
-    
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openApps: []
+    }
+  }
+
+  setAppOpen = (appName) => {
+    let currentState = this.state
+    this.setState({
+      openApps: [...currentState.openApps, appName]
+    })
+  }
+
+  
+
+
+  render() {
+    return (
+      <div>
+        <AppContext.Consumer>
+          <Desktop background='#D8AEB6' backgroundImg={logo}/>
+        </AppContext.Consumer>
+        
+        <AppContext.Provider>
+          <MenuBar />
+        </AppContext.Provider>
+      </div>
+    );
+  }
+  
 }
 
 export default App
