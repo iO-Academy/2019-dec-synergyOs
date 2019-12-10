f01b9e5ce1dd9fceaa68894f0ce4dd2557e81260
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-10 10:15:53 +0000

    reformatted app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Desktop from './Components/Desktop/Desktop';
 import logo from './res/synergyoslogo.png';
 
 function App() {
-  return (
-    <div>
-      <Desktop background='#D8AEB6' backgroundImg={logo}/>
-      <MenuBar />
-    </div>
-    
-  );
+    return (
+        <div>
+            <Desktop background='#D8AEB6' backgroundImg={logo}/>
+            <MenuBar/>
+        </div>
+
+    );
 }
 
 export default App
