3a528f88a35bfc846c429beb436166e0375e01fe
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-10 09:21:23 +0000

    reformatted fullscreen.js for readability

--- src/Components/Menu/Fullscreen.js
+++ src/Components/Menu/Fullscreen.js
@@ -8,17 +8,17 @@ const root = document.getElementById('root')
 const toggleFullscreen = () => {
 
     screenfull.toggle(root);
-        if (document.getElementById('fullscreenButton').src=fullscreenOpen) {
-            document.getElementById('fullscreenButton').src=fullscreenClose
+    if (document.getElementById('fullscreenButton').src = fullscreenOpen) {
+        document.getElementById('fullscreenButton').src = fullscreenClose
     } else {
-            document.getElementById('fullscreenButton').src=fullscreenOpen
-        }
+        document.getElementById('fullscreenButton').src = fullscreenOpen
+    }
 }
 
 const Fullscreen = () => {
 
     return (
-        <img id="fullscreenButton" src={fullscreenOpen} onClick={toggleFullscreen} />
+        <img id="fullscreenButton" src={fullscreenOpen} onClick={toggleFullscreen}/>
     )
 }
 
