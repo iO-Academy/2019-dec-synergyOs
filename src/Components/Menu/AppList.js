53a27f8da2933ac183afbd7d09b909e2721baef3
Anton Webb <anton25360@gmail.com>
2019-12-10 13:52:02 +0000

    fully Implemented icons.

--- src/Components/Menu/AppList.js
+++ src/Components/Menu/AppList.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './AppList.css'
 
+import About from '../../res/icons/about.svg'
+import Giphy from '../../res/icons/giphy.png'
+
 class AppList extends Component {
     render() {
+
+        let iconPath = ''
         const apps = Object.getOwnPropertyNames(this.props.appData.currentApps)
+        apps.forEach(element => {
+            switch (element) {
+                case 'About':
+                    iconPath = About
+                    break;
+
+                case 'Giphy':
+                    iconPath = Giphy
+                    break;
+
+                default:
+                    iconPath = About
+                    break;
+            }
+        });
+
         const listItems = apps.map((appName) =>
-            <li onClick={()=>this.props.appData.openApp(appName)} key={appName} className='AppListItem AppListHover'>{appName}</li>
+            <div className='AppContainer'>
+                <img className='AppIcon' src={iconPath} alt='icon' />
+                <li onClick={() => this.props.appData.openApp(appName)} key={appName} className='AppListItem'>{appName}</li>
+            </div>
         )
 
         return (
