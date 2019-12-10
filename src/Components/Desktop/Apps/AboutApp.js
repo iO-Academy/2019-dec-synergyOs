3a9e6f46693d7eede5b58fd828600ec89ae1e4c4
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-10 11:59:50 +0000

    reformatted AboutApp.js for readability

--- src/Components/Desktop/Apps/AboutApp.js
+++ src/Components/Desktop/Apps/AboutApp.js
@@ -8,7 +8,7 @@ import anton from '../../../res/headshots/anton.png'
 import josh from '../../../res/headshots/josh.png'
 
 class TestApp extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         let appName = this.props.name
         console.log(this.props.desktopState.currentApps[appName])
@@ -23,11 +23,11 @@ class TestApp extends React.Component {
         }
 
         this.props.desktopState.closeApp(appName)
-    
+
     }
 
     componentDidUpdate(prevProps) {
-        if(prevProps !== this.props) {
+        if (prevProps !== this.props) {
             this.setState({
                 name: this.state.name,
                 visibility: this.props.desktopState.currentApps[this.state.name],
@@ -36,7 +36,7 @@ class TestApp extends React.Component {
 
         let app = document.getElementById(this.state.name)
 
-        if(this.props.desktopState.activeApp === this.state.name) {
+        if (this.props.desktopState.activeApp === this.state.name) {
             app.classList.add('active')
         } else {
             app.classList.remove('active')
@@ -49,11 +49,18 @@ class TestApp extends React.Component {
         app.style.zIndex = zIndex
     }
 
-    render(){
+    render() {
         return (
-            <div onClick={this.activateApp} id={this.state.name} className={'app ' + this.state.visibility} style={this.style}>
-                <div className="topBar" onPointerDown={e => { dragApp(e.target, this.activateApp)}}>
-                    <button onClick={() => {this.props.desktopState.closeApp(this.state.name); console.log(this.state.name)}}>X</button>
+            <div onClick={this.activateApp} id={this.state.name} className={'app ' + this.state.visibility}
+                 style={this.style}>
+                <div className="topBar" onPointerDown={e => {
+                    dragApp(e.target, this.activateApp)
+                }}>
+                    <button onClick={() => {
+                        this.props.desktopState.closeApp(this.state.name);
+                        console.log(this.state.name)
+                    }}>X
+                    </button>
                     <div className="divider"></div>
                     <p>{this.state.name}</p>
                 </div>
@@ -63,19 +70,24 @@ class TestApp extends React.Component {
                     <div className='aboutContact'>
                         <img src={alex} alt='headshot' className='aboutHeadshot'/>
                         <a href='https://github.com/sudokufan' className='aboutName'>@sudokufan</a>
-                    </div><br/>
+                    </div>
+                    <br/>
                     <div className='aboutContact'>
                         <img src={anton} alt='headshot' className='aboutHeadshot'/>
                         <a href='https://github.com/anton25360' className='aboutName'>@anton25360</a>
-                    </div><br/>
+                    </div>
+                    <br/>
                     <div className='aboutContact'>
                         <img src={josh} alt='headshot' className='aboutHeadshot'/>
                         <a href='https://github.com/jdselby24' className='aboutName'>@jbselby24</a>
-                    </div><br/><br/>
+                    </div>
+                    <br/><br/>
                 </div>
-                
+
                 <div className="app-statusBar">
-                    <div onPointerDown={e => { resizeApp(e.target.parentElement, this.activateApp)}}></div>
+                    <div onPointerDown={e => {
+                        resizeApp(e.target.parentElement, this.activateApp)
+                    }}></div>
                 </div>
             </div>
         )
