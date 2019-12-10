7798874998d8d011d735b57c511e4a55deac2d97
jdselby24 <jdselby24@gmail.com>
2019-12-10 10:20:08 +0000

    Moved app drag function to external file

--- src/Components/Desktop/Apps/TestApp.js
+++ src/Components/Desktop/Apps/TestApp.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './Apps.css'
+import dragApp from './AppDragger'
+console.log(dragApp)
 
 class TestApp extends React.Component {
     constructor(props){
@@ -35,42 +37,6 @@ class TestApp extends React.Component {
         }
     }
 
-    dragApp(target) {
-        this.activateApp()
-        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
-        
-        target.onmousedown = dragMouseDown;
-        let elmnt = target.parentElement
-
-        function dragMouseDown(e) {
-            e = e || window.event;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        }
-
-        function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-          }
-    }
-
     activateApp = () => {
         let zIndex = this.props.desktopState.activateApp(this.state.name)
         let app = document.getElementById(this.state.name)
@@ -80,13 +46,16 @@ class TestApp extends React.Component {
     render(){
         return (
             <div onClick={this.activateApp} id={this.state.name} className={'app ' + this.state.visibility} style={this.style}>
-                <div className="topBar" onPointerDown={e => this.dragApp(e.target)}>
+                <div className="topBar" onPointerDown={e => { dragApp(e.target, this.activateApp)}}>
                     <button onClick={() => {this.props.desktopState.closeApp(this.state.name); console.log(this.state.name)}}>X</button>
                     <div className="divider"></div>
                     <p>{this.state.name}</p>
                 </div>
                 <div className="app-content">
                     <h1>TestApp</h1>
+                </div>
+                <div className="app-statusBar">
+
                 </div>
             </div>
         )
