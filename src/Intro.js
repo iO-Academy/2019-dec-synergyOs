023a1eec65b09cde8bb0cf34b7b98f478600b2b7
jdselby24 <jdselby24@gmail.com>
2019-12-11 18:41:47 +0000

    Made intro fullscreen

--- src/Intro.js
+++ src/Intro.js
@@ -13,7 +13,7 @@ class Intro extends Component{
 
     render() {
         return(
-            <div onLoad={(e) => {screenfull.request(document.querySelector('video')); document.querySelector('video').play(); }}>
+            <div onClick={(e) => {screenfull.request(document.querySelector('video')); document.querySelector('video').play(); }}>
                 <button id="start" onClick={(e) => {e.target.remove();screenfull.request(document.querySelector('video')); document.querySelector('video').play(); }}>START SYNERGY OS</button>
                 <div className="video">
                     <video onClick={endVid} onEnded={(e) => {screenfull.exit(); endVid()}} width='640px' height="100%" src={vid}type="video/mp4"></video>
