fe42eb5924a8f4b24f91eae5c810ed2429f8bde6
jdselby24 <jdselby24@gmail.com>
2019-12-11 21:08:55 +0000

    made app button a functional component

--- src/Components/Menu/AppBtn.js
+++ src/Components/Menu/AppBtn.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import circle from '../../res/circle.svg'
 
-class AppBtn extends React.Component {
-    render() {
+function AppBtn(){
 
-        const showAppList = () => {
-            var appList = document.querySelector('.AppList')
-            appList.style.display = appList.style.display === "block" ? "none" : "block"
-        }
-
-        return (
-            <div>
-                <img onClick={() => showAppList()} className='appBtn' src={circle} alt='app list'/>
-            </div>
-        )
+    const showAppList = () => {
+        var appList = document.querySelector('.AppList')
+        appList.style.display = appList.style.display === "block" ? "none" : "block"
     }
+
+    return(
+        <div>
+            <img onClick={() => showAppList()} className='appBtn' src={circle} alt='app list'/>
+        </div>
+    )
 }
 
 export default AppBtn
