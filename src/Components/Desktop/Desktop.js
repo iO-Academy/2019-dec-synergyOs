aa5d97e51b207f6e35b3680ce6148b12da56409d
jdselby24 <jdselby24@gmail.com>
2019-12-11 22:24:18 +0000

    added superdraw

--- src/Components/Desktop/Desktop.js
+++ src/Components/Desktop/Desktop.js
@@ -15,6 +15,7 @@ import Codepen from "./Apps/Codepen/Codepen"
 import SuperSpreadSheet from './Apps/Office/SuperSpreadSheet/SuperSpreadSheet'
 import SuperWriter from './Apps/Office/SuperWriter/SuperWriter'
 import SuperPresenter from './Apps/Office/SuperPresenter/SuperPresenter'
+import SuperDraw from './Apps/Office/SuperDraw/SuperDraw'
 
 class Desktop extends Component {
     constructor(props) {
@@ -64,7 +65,8 @@ class Desktop extends Component {
                 <Calculator name="Calculator" desktopState={desktopState}/>
                 <SuperWriter name="SuperWriter" desktopState={desktopState} />
                 <SuperSpreadSheet name="SuperSpreadSheet" desktopState={desktopState} />
-                <SuperPresenter name="SuperPresenter" desktopState={desktopState} />s
+                <SuperPresenter name="SuperPresenter" desktopState={desktopState} />
+                <SuperDraw name="SuperDraw" desktopState={desktopState} />s
                 <Nyan name="TBWA" desktopState={desktopState}/>
                 <Revelations name="TBWA" desktopState={desktopState}/>
                 <ThemesApp name="Themes" desktopState={desktopState}/>
